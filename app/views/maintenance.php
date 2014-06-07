37a750e72737a2049a4f77d844511a7907356a3b
Chester Li <yectep@gmail.com>
2014-06-06 20:01:23 -0700

    Initial commit

---
+++ app/views/maintenance.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>We'll be right back</title>
+    <style>
+        body {
+            font-family: 'Helvetica Neue', Helvetica, Arial, Tahoma, sans-serif;
+            font-size: 32px;
+            font-weight: 300;
+            color: #333;
+        }
+
+        .info {
+            margin: 25% auto 0 auto;
+            width: 600px;
+            text-align: center;
+        }
+
+        .small {
+            font-size: 22px;
+            color: #888;
+            margin-top: 1em;
+        }
+    </style>
+</head>
+<body>
+<div class="info">Just performing some maintenance.</div>
+<div class="small info">We'll be back in a just a moment.</div>
+</body>
+</html>
